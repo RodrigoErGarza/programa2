1b025e6299d9c9f8393b51138717c34c800fbb98
Walter Garcia <Walter Garcia@DESKTOP-ARKTHNQ>
2022-02-18 23:02:41 -0600

    Agregar archivos de proyecto.

---
+++ programa2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("HOLA!!");
+Console.WriteLine("Por favor ingresa dos numero: ");
+byte num1= byte.Parse(Console.ReadLine());
+byte num2= byte.Parse(Console.ReadLine());
+ushort multiplicacion = ((ushort)(num1 * num2));
+Console.WriteLine("El resultado de la multiplicación es: " + multiplicacion);
+Console.ReadKey();
